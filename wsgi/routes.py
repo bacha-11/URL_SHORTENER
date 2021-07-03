9f0cc77d1b5723a257baec2c871f63df9ee872d4
bacha-11 <ijazbacha14@gmail.com>
2021-07-03 15:41:00 +0500

    added static folder & Design a responsive index page

--- wsgi/routes.py
+++ wsgi/routes.py
@@ -3,25 +3,36 @@ from flask import render_template, redirect, url_for, request
 from wsgi.models import Link
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return render_template('index.html', title='Url Shortener')
 
+
+@app.route('/add-link', methods=['GET', 'POST'])
+def add_link():
     links = Link.query.all()
     if request.method == 'POST':
         original_url = request.form['original_url']
         add_url = Link(original_url=original_url)
         db.session.add(add_url)
         db.session.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('add_link'))
 
-    return render_template('index.html', links=links)
+    return render_template('add_link_and_state.html', links=links, title='Home')
 
 
 @app.route('/<short_url>')
 def redirect_to_url(short_url):
-    link = Link.query.filter_by(short_url=short_url).first()
+    link = Link.query.filter_by(short_url=short_url).first_or_404()
     if link:
         link.visite = link.visite + 1
         db.session.commit()
         return redirect(link.original_url)
+
+
+
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return '<h1>Page Not Found - Plaease Enter a Vaild Url', 404
         

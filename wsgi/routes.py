d9eb81d51cc2641d9ba0587da5d0729ef643b899
bacha-11 <ijazbacha14@gmail.com>
2021-07-05 11:48:20 +0500

    Design a responsive links page

--- wsgi/routes.py
+++ wsgi/routes.py
@@ -10,7 +10,7 @@ def index():
 
 @app.route('/add-link', methods=['GET', 'POST'])
 def add_link():
-    links = Link.query.all()[0:7]
+    links = Link.query.order_by(Link.date_create.desc()).all()[0:6]
     if request.method == 'POST':
         original_url = request.form['original_url']
         add_url = Link(original_url=original_url)
@@ -19,6 +19,7 @@ def add_link():
         return redirect(url_for('add_link'))
 
     return render_template('add_link_and_state.html', links=links, title='Citly | Url Shotener')
+
 
 
 @app.route('/<short_url>')
@@ -31,8 +32,23 @@ def redirect_to_url(short_url):
 
 
 
+@app.route('/remove_url/<id>')
+def remove_url(id):
+    link = Link.query.filter_by(id=id).first_or_404()
+    if link:
+        db.session.delete(link)
+        db.session.commit()
+        return redirect(url_for('add_link'))
 
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return '<h1>Page Not Found - Plaease Enter a Vaild Url', 404
+
+@app.route('/list-of-links')
+def list_of_links():
+    links = Link.query.all()
+    return render_template('list_of_links.html', title='Citly Links', links=links)
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return '<h1>Page Not Found - Plaease Enter a Vaild Url', 404
         
